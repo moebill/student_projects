ecb59f76c72b1d4755961165d81fd5e0a814c3f9
moebill <vladik1098273645@gmail.com>
2025-11-19 12:11:15 +0000

    studentmachine send

--- cpp/szablon/main.cpp
+++ cpp/szablon/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-
-using namespace std;
-int main(){
+ using namespace std;
+/* int main(){
 int x;
 cout<<"podaj x:";
 cin>>x;	
@@ -13,3 +12,24 @@ cout<<"nie podzielna przez 21"<<endl;
 return 0;
 }
 
+*/
+
+int main(){
+int n;
+cout<< "podaj liczby:";
+cin >> n;
+bool pierwsza = true;
+
+if(n <= 1)pierwsza = false;
+
+for(int i = 2; i < n; i++){
+if(n % i == 0){
+pierwsza = false;
+break;
+}
+}
+
+if(pierwsza) cout<<"liczba pierwsza";
+else cout<<"nie jest pierwsza";
+return 0;
+}
