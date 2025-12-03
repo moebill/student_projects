5fe3d7f62cffa5cd26d425a6d3f4aa1f7bfbd375
moebill <student@student.machine>
2025-12-03 12:11:35 +0000

    studentmachine send

--- cpp/szablon/main.cpp
+++ cpp/szablon/main.cpp
@@ -36,7 +36,7 @@ if(pierwsza) cout<<"liczba pierwsza";
 else cout<<"nie jest pierwsza";
 return 0;
 }
-*/
+
 
 int main(){
 srand(time(0));
@@ -44,5 +44,28 @@ int liczba1 = rand() % 10 + 1;
 int liczba2 = rand() % 100 + 50;
 std::cout<<"losowa liczba z zakresu [1,10]:"<<liczba1<<std::endl;
 std::cout<<"losowana liczba z zakresu [100,50]:"<<liczba2<<std::endl;
+return 0;
+}
+*/
+#include <iostream>
+
+int main(){
+const int x = 3;
+const int y = 4;
+const int z = 5;
+char tab[x][y][z];
+for (int i=0; i<x;++i)
+for (int j=0; j<y;++j)
+for (int k=0; k<z; ++k)
+tab[i][j][k] = 'w';
+for (int i = 0; i<x; ++i){
+std::cout <<"warstwa"<< i <<":\n";
+for (int j = 0;j<x;++i){
+for (int k = 0; k<z; ++k)
+std::cout<<tab[i][j][k];
+std::cout<<'\n';
+}
+std::cout<<'\n';
+}
 return 0;
 }
